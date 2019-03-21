2b1bb34b294164cf91e21bc3383f22ef41a467d2
surf <surf@skivers.co.uk>
2019-03-21 22:17:50 +0000

    format code, tidy up

--- public/translationProcessor.php
+++ public/translationProcessor.php
@@ -1,93 +1,104 @@
 <?php
 
-class TranslationProcessor {
+class TranslationProcessor
+{
+	private $jsonFile;
+	protected $timeCodes;
+	protected $translation;
+	protected $wordTimeCodes;
+	private $dataFromFile;
 
-    private $jsonfile;
-    protected $timeCodes;
-    protected $tranlation;
-    protected $wordTimeCodes;
-    private $dataFromFile;
-    
-    
-    public function __construct(string $jsonFile){
-        $this->jsonfile = $jsonFile;
-    }
+	/**
+	 * TranslationProcessor constructor.
+	 * @param string $jsonFile
+	 */
+	public function __construct(string $jsonFile)
+	{
+		$this->jsonFile = $jsonFile;
+	}
 
-    public function translateTextWithTimeCodes()
-    {
-        $this->dataFromFile = $this->getDecodedJsonFileContents();
+	/**
+	 * @return $this
+	 */
+	public function translateTextWithTimeCodes()
+	{
+		$this->dataFromFile = $this->getDecodedJsonFileContents();
+		$this->setTimeCodes();
+		$this->setWordTimeCode();
+		$this->setTranslation();
+		$this->mapTimecodesAndTranslation();
 
-        
-        $this->setTimeCodes();
-        $this->setWordTimeCode();
-        $this->setTranslation();
-        $this->mapTimecodesAndTranslation();
-      
-        return $this;
-    }
+		return $this;
+	}
 
+	private function mapTimecodesAndTranslation()
+	{
+		$translationArray = $this->getTranslationArray();
 
-    private function mapTimecodesAndTranslation()
-    {
-        $translationArray = $this->getTranslationArray();
+		foreach ($this->timeCodes as $key => &$timeCodes) {
+			$timeCodes['translation'] = $translationArray[$key];
+		}
+	}
 
-        foreach($this->timeCodes as $key =>  &$timeCodes)
-        {
-            $timeCodes['translation'] = $translationArray[$key];
-        }
-        
-    }
+	/**
+	 * @return string
+	 */
+	public function toJson(): string
+	{
+		return html_entity_decode(
+			json_encode(
+				$this->timeCodes
+			)
+		);
+	}
 
-    public function toJson()
-    {
-        return html_entity_decode(
-            json_encode(
-                $this->timeCodes
-            )
-        );
-    }
+	/**
+	 * @return array
+	 */
+	public function toArray(): array
+	{
+		return $this->timeCodes;
+	}
 
-    public function toArray()
-    {
-        return $this->timeCodes;
-    }
+	/**
+	 * @return array
+	 */
+	private function getTranslationArray(): array
+	{
+		$results = array_filter(explode('.', $this->translation));
+		foreach ($results as &$sentence) {
+			$sentence .= ".";
+		}
 
-    private function getTranslationArray(): array
-    {
-        $results =  array_filter(explode('.',$this->translation));
-        foreach($results as &$sentence){
-            $sentence .= ".";            
-        }
-        return $results;
-    }
+		return $results;
+	}
 
+	/**
+	 * @return mixed
+	 */
+	private function getDecodedJsonFileContents()
+	{
+		return json_decode(
+			htmlentities(
+				file_get_contents($this->jsonFile),
+				ENT_NOQUOTES
+			),
+			true
+		);
+	}
 
-    private function getDecodedJsonFileContents()
-    {
-        return  json_decode(
-                    htmlentities
-                    (
-                        file_get_contents($this->jsonfile),
-                        ENT_NOQUOTES
-                    ),
-            true
-        );
+	private function setTimeCodes()
+	{
+		$this->timeCodes = $this->dataFromFile['time_codes'] ?? [];
+	}
 
-    }
+	private function setTranslation()
+	{
+		$this->translation = $this->dataFromFile['translation'] ?? [];
+	}
 
-    private function setTimeCodes(){
-        $this->timeCodes = $this->dataFromFile['time_codes'] ?? [] ;
-       
-    }
-
-    private function setTranslation(){
-        $this->translation = $this->dataFromFile['translation'] ?? [] ;
-        
-    }
-
-
-    private function setWordTimeCode(){
-        $this->wordTimeCodes = $this->dataFromFile['word_time_codes'] ?? [] ;
-    }
-
+	private function setWordTimeCode()
+	{
+		$this->wordTimeCodes = $this->dataFromFile['word_time_codes'] ?? [];
+	}
 }
